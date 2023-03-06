6f61d10050c8792e1bb3714e910e031a0bbf2492
xFranv8 <32195567+xFranv8@users.noreply.github.com>
2023-03-06 16:14:05 +0100

    Update settings.py
    Revoke private django key

--- WebApplication/CarDensityAI/settings.py
+++ WebApplication/CarDensityAI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-np@*zvt6^h@p-*w$zg^jkuo9=c*9(9-f_j&kgzcjr97xut%p#o'
+SECRET_KEY = @yourkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
