e45d66de2312248215d3b72dfcaa782df40d0a5f
xfranv8 <fc.vazquez.2018@alumnos.urjc.es>
2023-03-29 13:33:07 +0200

    Issue https://github.com/xFranv8/AIVA_2023_COCHES/issues/8 solved

--- ResultViewer.py
+++ ResultViewer.py
@@ -5,34 +5,34 @@ import numpy as np
 
 class ResultViewer:
     @staticmethod
-    def counter(rectangles:list[BoundingBox]):
+    def counter(rectangles: list[BoundingBox]):
         return len(rectangles)
 
     @staticmethod
-    def draw_results(image:np.array,rectangles:list[BoundingBox],SIZE:int):
+    def draw_results(image: np.ndarray, rectangles: list[BoundingBox], SIZE: int):
         for rectangle in rectangles:
             n: int = rectangle.patch_number
-            cv2.rectangle(image, (rectangle.point1[0] + (n % 40) * SIZE, rectangle.point1[1] + (n // 40) * SIZE),
-                          (rectangle.point2[0] + (n % 40) * SIZE, rectangle.point2[1] + (n // 40) * SIZE), (0, 255, 0), 2)
+            length: int = image.shape[0]
 
-        cv2.imshow(image)
+            cv2.rectangle(image, (rectangle.point1[0] + (n % (length//SIZE)) * SIZE, rectangle.point1[1] + (n // (length//SIZE)) * SIZE),
+                          (rectangle.point2[0] + (n % (length//SIZE)) * SIZE, rectangle.point2[1] + (n // (length//SIZE)) * SIZE), (0, 255, 0), 1)
+
+        cv2.imwrite("detection.png", image)
 
         aux = cv2.waitKey(0)
 
     @staticmethod
-    def write_csv(rectangles:list[BoundingBox]):
+    def write_csv(rectangles: list[BoundingBox]):
         cars_count = ResultViewer.counter(rectangles)
-        cars_detected= ['Cars detected', cars_count]
+        cars_detected = ['Cars detected', cars_count]
         filename = 'my_file.csv'
 
         with open(filename, mode='w', newline='') as csv_file:
             writer = csv.writer(csv_file)
 
             writer.writerow(cars_detected)
-            writer.writerow('BBox')
-
 
             for rectangle in rectangles:
-                writer.writerow([rectangle.point1[0],rectangle.point1[1]])
+                writer.writerow([rectangle.point1[0], rectangle.point1[1]])
 
         print(f"{filename} has been created successfully!")
