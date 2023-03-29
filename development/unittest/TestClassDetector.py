9ece5b87a187087fe09dbfead619ae27ff9510dd
xfranv8 <fc.vazquez.2018@alumnos.urjc.es>
2023-03-29 13:46:08 +0200

    Reorganized the code

--- development/unittest/TestClassDetector.py
+++ development/unittest/TestClassDetector.py
@@ -9,24 +9,24 @@ Autores: Blanca Rodríguez González (b.rodriguezg.2018@alumnos.urjc.es)
 
 """
 
-from ImagePatcher import ImagePatcher
+from CarDetector import CarDetector
 import cv2
 import numpy as np
 import unittest
 
 
-class TestImagePatcher(unittest.TestCase):
+class TestCarDetector(unittest.TestCase):
     def setUp(self):
-        self.car_detector = ImagePatcher((1024, 1024, 3), (512, 512, 3))
+        self.car_detector = CarDetector((1024, 1024, 3), (512, 512, 3))
 
     def test_patcher(self):
         image: np.array = np.array(cv2.imread("test.png"))
 
         # Prueba: Número de patches correcto
-        self.assertEqual(image.shape[0]/512, len(self.car_detector.patch_image(image)))
+        self.assertEqual(image.shape[0]/512, len(self.car_detector._patch_image(image)))
 
         # Prueba: Dimensión de la imagen reconstruida = Dimensión imagen original
-        reconstructered_images = self.car_detector.unpatch_images(self.car_detector.patch_image(image))
+        reconstructered_images = self.car_detector._unpatch_image(self.car_detector._patch_image(image))
 
         self.assertEqual(True, (reconstructered_images == image).all())
 
