40f65c922335bfbcbd17c408f9ee8f07cfbf6fac
xfranv8 <fc.vazquez.2018@alumnos.urjc.es>
2023-03-29 15:47:41 +0200

    This commit solves issue https://github.com/xFranv8/AIVA_2023_COCHES/issues/9

--- development/CarDensityAI.py
+++ development/CarDensityAI.py
@@ -7,20 +7,21 @@ from ResultViewer import ResultViewer
 
 
 class CarDensityAI:
-    def __init__(self, image_path: str, size: int) -> None:
+    def __init__(self, image_path: str, size: int, output_path: str) -> None:
         if not os.path.exists(image_path):
             raise Exception("File not found.")
         
         self.__image: np.ndarray = Reader.read_image(image_path)
         self.__SIZE: int = size
         self.__detector = Detector(self.__image.shape, (self.__SIZE, self.__SIZE, 3))
+        self.__output_path: str = output_path
     
     def main(self) -> None:
         rectangles: list[BoundingBox] = self.__detector.detect(self.__image)
-        ResultViewer.draw_results(self.__image, rectangles, self.__SIZE)
-        ResultViewer.write_csv(rectangles)
+        ResultViewer.draw_results(self.__image, rectangles, self.__SIZE, self.__output_path)
+        ResultViewer.write_csv(rectangles, self.__output_path, self.__SIZE, self.__image.shape[0])
 
 
 if __name__ == "__main__":
-    cardensityai = CarDensityAI("../images/chicago1.tif", 250)
+    cardensityai = CarDensityAI("../images/chicago1.tif", 250, "chicago1.csv")
     cardensityai.main()
--- development/ResultViewer.py
+++ development/ResultViewer.py
@@ -3,29 +3,30 @@ import cv2
 import csv
 import numpy as np
 
+
 class ResultViewer:
     @staticmethod
     def counter(rectangles: list[BoundingBox]):
         return len(rectangles)
 
     @staticmethod
-    def draw_results(image: np.ndarray, rectangles: list[BoundingBox], SIZE: int):
+    def draw_results(image: np.ndarray, rectangles: list[BoundingBox], SIZE: int, output_path: str):
+        LENGTH: int = image.shape[0]
         for rectangle in rectangles:
             n: int = rectangle.patch_number
-            length: int = image.shape[0]
 
-            cv2.rectangle(image, (rectangle.point1[0] + (n % (length//SIZE)) * SIZE, rectangle.point1[1] + (n // (length//SIZE)) * SIZE),
-                          (rectangle.point2[0] + (n % (length//SIZE)) * SIZE, rectangle.point2[1] + (n // (length//SIZE)) * SIZE), (0, 255, 0), 1)
+            cv2.rectangle(image, (ResultViewer.__local_to_global(rectangle.point1, n, SIZE, LENGTH)), (ResultViewer.__local_to_global(rectangle.point2, n, SIZE, LENGTH)), (0, 255, 0), 1)
 
-        cv2.imwrite("detection.png", image)
+        cv2.imwrite(f"{output_path.split('.')[0]}.png", image)
 
         aux = cv2.waitKey(0)
 
     @staticmethod
-    def write_csv(rectangles: list[BoundingBox]):
+    def write_csv(rectangles: list[BoundingBox], output_path: str, SIZE: int, LENGTH: int):
         cars_count = ResultViewer.counter(rectangles)
         cars_detected = ['Cars detected', cars_count]
-        filename = 'my_file.csv'
+
+        filename: str = output_path.split('.')[0] + ".csv"
 
         with open(filename, mode='w', newline='') as csv_file:
             writer = csv.writer(csv_file)
@@ -33,6 +34,14 @@ class ResultViewer:
             writer.writerow(cars_detected)
 
             for rectangle in rectangles:
-                writer.writerow([rectangle.point1[0], rectangle.point1[1]])
+                n: int = rectangle.patch_number
+                pt1: tuple[int, int] = ResultViewer.__local_to_global(rectangle.point1, n, SIZE, LENGTH)
+                pt2: tuple[int, int] = ResultViewer.__local_to_global(rectangle.point2, n, SIZE, LENGTH)
+
+                writer.writerow([pt1[0], pt1[1], pt2[0], pt2[1]])
 
         print(f"{filename} has been created successfully!")
+    
+    @staticmethod
+    def __local_to_global(point: tuple[int, int], patch_number: int, SIZE: int, length: int):
+        return point[0] + (patch_number % (length//SIZE)) * SIZE, point[1] + (patch_number // (length//SIZE)) * SIZE
